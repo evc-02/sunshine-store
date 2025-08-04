fb2811023325d454dbdd470b9681d76cc92d02ff
evc-02 <edelvecar@gmail.com>
2025-08-04 17:57:07 -0400

    Creación de Registros Tabla Categorías

---
+++ backend/backend/database/seeders/CategoriaSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Insertar Registros a la tabla
+
+        DB::table('categorias')->insert([
+            [
+                'nombre'=> 'Ropa',
+                'descripcion'=> 'Ropa Infantil',
+                'created_at'=> now(),
+                'updated_at'=> now(),
+            ], 
+            [
+                'nombre'=> 'Accesorios',
+                'descripcion'=> 'Accesorios Infantiles',
+                'created_at'=> now(),
+                'updated_at'=> now(),
+            ], 
+        ]);
+    }
+}
