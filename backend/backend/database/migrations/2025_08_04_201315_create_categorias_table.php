a3b3816b309f608f160057c1b6f84e054c825cde
evc-02 <edelvecar@gmail.com>
2025-08-04 16:48:32 -0400

    Creación de Tabla Categorías y sus campos

--- backend/backend/database/migrations/2025_08_04_201315_create_categorias_table.php
+++ backend/backend/database/migrations/2025_08_04_201315_create_categorias_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre',50);
+            $table->string('descripcion',100);
             $table->timestamps();
         });
     }
